fa6194a049608638fc86806e730a4900351c8e09
mahmudin muttaqin <claxmanx@gmail.com>
2019-12-26 12:49:54 +0700

    buat form dengan formik dan yup

---
+++ src/components/Formik/TextInput.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useField } from 'formik';
+
+export const TextInput = ({ label, ...props }) => {
+	const [field, meta] = useField(props);
+	return (
+		<>
+			<div className="form-group">
+				<label 
+					className="form-label"
+					htmlFor={props.id || props.name}>{label}</label>
+				<input  
+			        className={
+			          meta.touched && meta.error ? 'form-control is-invalid' : 'form-control'
+			        }				
+			        {...field}
+			        {...props}
+				/>
+				{meta.touched && meta.error ? (
+					<div className="invalid-feedback">{meta.error}</div>
+				) : null}
+				</div>	
+		</>
+	)
+} 
--- src/containers/pages/Login/LoginForm.js
+++ src/containers/pages/Login/LoginForm.js
@@ -1,68 +1,42 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, Form, useFormik, useField } from 'formik';
 import * as Yup from 'yup';
+import {TextInput} from '../../../components/Formik/TextInput';
 
 const LoginForm = () => {
-	const formik = useFormik({
-		initialValues: {
-			email: '',
-			password: ''
-		},
-		validationSchema: Yup.object({
-			email: Yup.string()
-				.email('Invalida email address')
-				.required('Required'),
-			password: Yup.string()
-				.required('Required')
-		}),
-		onSubmit: values => {
-			console.log(values)
-		}
-	})
-
 	return (
-		<form onSubmit={formik.handleSubmit}>
-			<div className="form-group">
-				<label 
-					className="form-label"
-					htmlFor="email">Email</label>
-				<input 
-			        className={
-			          formik.touched.email && formik.errors.email ? 'form-control is-invalid' : 'form-control'
-			        }
+		<Formik
+			initialValues={{
+				email: '',
+				password: ''			
+			}}
+			validationSchema={Yup.object({
+				email: Yup.string()
+					.email('Invalida email address')
+					.required('Required'),
+				password: Yup.string()
+					.required('Required')
+			})}
+			onSubmit={(values, { setSubmitting }) => {
+				console.log(values)
+			}}
+		>
+			<Form>
+				<TextInput 
+					label="Email"
 					type="email"
 					name="email"
-					id="email"
-					onChange={formik.handleChange}
-					onBlur={formik.handleBlur}
-					value={formik.values.email}
+					placeholder="Enter your email here"
 				/>
-				{formik.touched.email && formik.errors.email ? (
-					<div className="invalid-feedback">{formik.errors.email}</div>
-				) : null}
-			</div>
-
-			<div className="form-group">
-				<label 
-					className="form-label"
-					htmlFor="password">Password</label>
-				<input  
-			        className={
-			          formik.touched.email && formik.errors.email ? 'form-control is-invalid' : 'form-control'
-			        }				
+				<TextInput 
+					label="Password"
 					type="password"
 					name="password"
-					id="password"
-					onChange={formik.handleChange}
-					onBlur={formik.handleBlur}
-					value={formik.values.password}
+					placeholder="Enter your password here"
 				/>
-				{formik.touched.password && formik.errors.password ? (
-					<div className="invalid-feedback">{formik.errors.password}</div>
-				) : null}
-				</div>
-			<button type="submit">LOGIN</button>
-		</form>
+				<button type="submit" className="btn btn-primary btn-block">LOGIN</button>
+			</Form>	
+		</Formik>
 	)
 }
 
